21a4e9679f5cd1c16e0332d5d3b86ad7dd7b9f30
The Data Wizards <124339597+datawizards-vbo@users.noreply.github.com>
2023-02-20 07:54:47 +0100

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,40 @@
+# Data Types in Python
+# numbers (sayılar)
+a = 5
+b = 5.5
+c = 5 + 5j
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'float'>
+print(type(c)) # <class 'complex'>
+
+# strings (metinler)
+d = "hello"
+e = 'world'
+print(type(d)) # <class 'str'>
+print(type(e)) # <class 'str'>
+
+# lists (listeler)
+f = [1, 2, 3, 4, 5]
+print(type(f)) # <class 'list'>
+
+# tuples (demetler)
+g = (1, 2, 3, 4, 5)
+print(type(g)) # <class 'tuple'>
+
+# sets (kümeler)
+h = {1, 2, 3, 4, 5}
+print(type(h)) # <class 'set'>
+
+# dictionaries (sözlükler)
+i = {"name": "John", "age": 30}
+print(type(i)) # <class 'dict'>
+
+# booleans (booleanlar)
+j = True
+k = False
+print(type(j)) # <class 'bool'>
+print(type(k)) # <class 'bool'>
+
+# NoneType
+l = None
+print(type(l)) # <class 'NoneType'>
