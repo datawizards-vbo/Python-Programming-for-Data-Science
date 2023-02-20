3706007810f330acb5a5975a66a03758aca8b8f0
The Data Wizards <124339597+datawizards-vbo@users.noreply.github.com>
2023-02-20 07:50:06 +0100

    Add files via upload

---
+++ conda.py
@@ -0,0 +1,42 @@
+# Creating a virtual environment named DSMLBC11
+# conda create -n DSMLBC11
+
+# Activating the virtual environment named DSMLBC11
+# conda activate DSMLBC11
+
+# Listing all available virtual environments
+# conda env list
+
+# Deactivating the active virtual environment
+# conda deactivate
+
+# Removing the virtual environment named new_env
+# conda env remove -n DSMLBC11
+
+# Listing all installed packages
+# conda list
+
+# Installing a package named numpy
+# conda install numpy
+
+# Installing multiple packages named numpy, pandas, and scipy
+# conda install numpy pandas scipy
+
+# Installing a specific version of numpy
+# conda install numpy=1.20.1
+
+# Upgrading a package named numpy
+# conda upgrade numpy
+
+# Upgrading all packages in the current environment
+# conda upgrade -all
+
+# Removing a package named numpy
+# conda remove numpy
+
+# Creating a virtual environment from an environment.yaml file
+# conda env create -f environment.yaml
+
+# Exporting the current environment's packages to an environment.yaml file
+# conda env export > environment.yaml
+
